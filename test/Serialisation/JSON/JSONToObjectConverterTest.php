9114e6f3a074af42bec1aeb329d08dd9a33a8bff
Mark Robertshaw <mark@oxil.co.uk>
2019-07-16 11:29:09 +0100

    Fixed tests.

--- test/Serialisation/JSON/JSONToObjectConverterTest.php
+++ test/Serialisation/JSON/JSONToObjectConverterTest.php
@@ -4,6 +4,7 @@
 namespace Kinikit\Core\Serialisation\JSON;
 
 
+use Kinikit\Core\DependencyInjection\Container;
 use Kinikit\Core\Exception\ClassNotConstructableException;
 use Kinikit\Core\Exception\ClassNotFoundException;
 use Kinikit\Core\Exception\ClassNotSerialisableException;
@@ -23,7 +24,7 @@ class JSONToObjectConverterTest extends \PHPUnit\Framework\TestCase {
         $boolean = true;
         $string = '"My Friend Emma"';
 
-        $converter = new JSONToObjectConverter ();
+        $converter = Container::instance()->get(JSONToObjectConverter::class);
         $this->assertEquals($int, $converter->convert($int));
         $this->assertEquals(1, $converter->convert($boolean));
         $this->assertEquals("My Friend Emma", $converter->convert($string));
@@ -35,7 +36,7 @@ class JSONToObjectConverterTest extends \PHPUnit\Framework\TestCase {
         $json = '[1,3,5,7,9,11]';
         $json2 = '["Mark","Amy","Peter","Paul"]';
 
-        $converter = new JSONToObjectConverter ();
+        $converter = Container::instance()->get(JSONToObjectConverter::class);
         $this->assertEquals(array(1, 3, 5, 7, 9, 11), $converter->convert($json));
         $this->assertEquals(array("Mark", "Amy", "Peter", "Paul"), $converter->convert($json2));
 
@@ -46,7 +47,7 @@ class JSONToObjectConverterTest extends \PHPUnit\Framework\TestCase {
         $json = '{"a" :1, "b":5, "c":"penny"}';
         $json2 = '{"Bad":"Bunny", "5":"Flag", "Seven":"England"}';
 
-        $converter = new JSONToObjectConverter ();
+        $converter = Container::instance()->get(JSONToObjectConverter::class);
         $this->assertEquals(array("a" => 1, "b" => 5, "c" => "penny"), $converter->convert($json));
         $this->assertEquals(array("Bad" => "Bunny", 5 => "Flag", "Seven" => "England"), $converter->convert($json2));
 
