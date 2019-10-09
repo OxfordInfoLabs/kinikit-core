1c4e6f4d94bba0f277dd9f2d6ac0a777fbe28b76
Mark Robertshaw <mark@oxil.co.uk>
2019-10-09 15:03:00 +0100

    New integer handling

--- src/Util/Primitive.php
+++ src/Util/Primitive.php
@@ -47,7 +47,7 @@ class Primitive {
             case self::TYPE_INTEGER:
                 return is_int($value) || is_numeric($value) && is_int(+$value);
             case self::TYPE_FLOAT:
-                return is_float($value) || is_int($value) || is_numeric($value) && is_float(+$value);
+                return is_float($value) || self::isPrimitive(self::TYPE_INT, $value) || is_numeric($value) && is_float(+$value);
             case self::TYPE_STRING:
                 return is_string($value) || is_numeric($value) || is_float($value) || is_bool($value);
             case self::TYPE_MIXED:
