e154e788b205bb34c7b9a7b9ba04cc3b1fbccd16
PeterSpainOXIL <114655461+PeterSpainOXIL@users.noreply.github.com>
2023-11-21 15:08:05 +0000

    Update trigger_travis_build.sh

--- trigger_travis_build.sh
+++ trigger_travis_build.sh
@@ -1,13 +1,10 @@
-body='{
-"request": {
-"branch":"master",
-"message":"Triggered by kinikit-core"
-}}'
-
 curl -X POST \
    -H "Content-Type: application/json" \
    -H "Travis-API-Version: 3" \
    -H "Authorization: token $TRAVIS_API_TOKEN" \
-   -d "$body" \
+   -d '{
+      "request": {
+      "branch":"master",
+      "message":"Triggered by kinikit-core"
+      }}' \
    https://api.travis-ci.com/repo/OxfordInfoLabs%2F$1/requests
-
