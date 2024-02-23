6417089ed32b2a9b57c16ae8283116a28fbffd4d
peteroxil <peter@oxil.co.uk>
2024-02-23 10:24:18 +0000

    Small refactor

--- src/Template/ValueFunction/StringValueFunction.php
+++ src/Template/ValueFunction/StringValueFunction.php
@@ -45,94 +45,94 @@ class StringValueFunction extends ValueFunctionWithArguments {
      */
     protected function applyFunctionWithArgs($functionName, $functionArgs, $value, $model) {
 
-        if (is_string($value)) {
-
-            switch ($functionName) {
-                case "substring":
-                    $offset = $functionArgs[0];
-                    $length = $functionArgs[1] ?? null;
-
-                    if ($length) {
-                        return substr($value, $offset, $length);
-                    } else {
-                        return substr($value, $offset);
-                    }
-
-                case "concat":
-                    $string = $value;
-                    foreach ($functionArgs as $arg) {
-                        $string .= $arg;
-                    }
-
-                    return $string;
-
-                case "toUTF8":
-                    return preg_replace('/(\xF0\x9F[\x00-\xFF][\x00-\xFF])|(\xF0\x93[\x80-\x89][\x00-\xFF])|(\xF0\x93\x90[\x00-\xAF])/', "", $value) == $value ? $value : null;
-
-                case "trim":
-                    return trim($value, $functionArgs[0]);
-
-                case "split":
-                case "explode":
-                    return explode($functionArgs[0], $value);
-
-                case "replace":
-                    $search = $functionArgs[0] ?? null;
-                    $replace = $functionArgs[1] ?? null;
-
-                    if (substr($search, 0, 1) == "/" &&
-                        substr($search, -1, 1) == "/") {
-
-                        return preg_replace($search, $replace, $value);
-                    } else {
-                        return str_replace($search, $replace, $value);
-                    }
-
-                case "contains":
-                    return (bool)strpos($value, $functionArgs[0]);
-
-                case "lowercase":
-                    return strtolower($value);
-
-                case "uppercase":
-                    return strtoupper($value);
-
-                case "append":
-                    foreach ($functionArgs as $arg) {
-                        $value .= $arg;
-                    }
-                    return $value;
-
-                case "prepend":
-                    foreach ($functionArgs as $arg) {
-                        $value = $arg . $value;
-                    }
-                    return $value;
-
-                case "initialCaps":
-                    return strlen($value) > 1 ? strtoupper(substr($value, 0, 1)) . strtolower(substr($value, 1)) : strtoupper($value);
-
-                case "words":
-                    return explode(" ", $value);
-
-                case "hash":
-                    return hash("sha512", $value);
-
-                case "md5":
-                    return md5($value);
-
-                case "startsWith":
-                    return substr($value, 0, 1);
-
-                case "endsWith":
-                    return substr($value, -1);
-            }
-
-            return $value;
-
-        } else {
+        if (!is_string($value)) {
             return $value;
         }
+
+        switch ($functionName) {
+            case "substring":
+                $offset = $functionArgs[0];
+                $length = $functionArgs[1] ?? null;
+
+                if ($length) {
+                    return substr($value, $offset, $length);
+                } else {
+                    return substr($value, $offset);
+                }
+
+            case "concat":
+                $string = $value;
+                foreach ($functionArgs as $arg) {
+                    $string .= $arg;
+                }
+
+                return $string;
+
+            case "toUTF8":
+                return preg_replace('/(\xF0\x9F[\x00-\xFF][\x00-\xFF])|(\xF0\x93[\x80-\x89][\x00-\xFF])|(\xF0\x93\x90[\x00-\xAF])/', "", $value) == $value ? $value : null;
+
+            case "trim":
+                return trim($value, $functionArgs[0]);
+
+            case "split":
+            case "explode":
+                return explode($functionArgs[0], $value);
+
+            case "replace":
+                $search = $functionArgs[0] ?? null;
+                $replace = $functionArgs[1] ?? null;
+
+                if (substr($search, 0, 1) == "/" &&
+                    substr($search, -1, 1) == "/") {
+
+                    return preg_replace($search, $replace, $value);
+                } else {
+                    return str_replace($search, $replace, $value);
+                }
+
+            case "contains":
+                return (bool)strpos($value, $functionArgs[0]);
+
+            case "lowercase":
+                return strtolower($value);
+
+            case "uppercase":
+                return strtoupper($value);
+
+            case "append":
+                foreach ($functionArgs as $arg) {
+                    $value .= $arg;
+                }
+                return $value;
+
+            case "prepend":
+                foreach ($functionArgs as $arg) {
+                    $value = $arg . $value;
+                }
+                return $value;
+
+            case "initialCaps":
+                return strlen($value) > 1 ? strtoupper(substr($value, 0, 1)) . strtolower(substr($value, 1)) : strtoupper($value);
+
+            case "words":
+                return explode(" ", $value);
+
+            case "hash":
+                return hash("sha512", $value);
+
+            case "md5":
+                return md5($value);
+
+            case "startsWith":
+                return substr($value, 0, 1);
+
+            case "endsWith":
+                return substr($value, -1);
+        }
+
+        return $value;
+
+
 
     }
 
