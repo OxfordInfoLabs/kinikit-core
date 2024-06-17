2fa43a3494200786ac5c33fa37346c8713c35011
peteroxil <peter@oxil.co.uk>
2024-06-17 12:35:29 +0100

    Tweak

--- src/HTTP/Dispatcher/AMPRequestDispatcher.php
+++ src/HTTP/Dispatcher/AMPRequestDispatcher.php
@@ -10,6 +10,7 @@ use Kinikit\Core\HTTP\Request\Request;
 use Kinikit\Core\HTTP\Response\Headers;
 use Kinikit\Core\HTTP\Response\Response;
 
+use Kinikit\Core\Logging\Logger;
 use Kinikit\Core\Stream\String\ReadOnlyStringStream;
 
 class AMPRequestDispatcher implements HttpRequestDispatcher {
